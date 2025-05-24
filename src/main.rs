c9324a7bcae1cc649275d7a064e7392aa6da4f2f
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-05-23 22:49:20 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use sycamore::prelude::*;
+
+#[component]
+fn App() -> View {
+    let lead_dev: &str = "Puppet";
+    let website_dev: &str = "Puppet";
+
+    view! {
+        div(id="main-div") {
+            h1(style="padding-top: 200px; padding-bottom: 5px;") {
+                "Geometry Rays"
+            }
+
+            h2(style="padding-top: 10px; font-size: 40px; color: #ff6a00;") {
+                "Fyre"
+            }
+
+            p {
+                (format!("Lead Dev: {}", lead_dev))
+            }
+
+            p {
+                (format!("Website made by: {}", website_dev))
+            }
+
+            h2 {
+                "Team"
+            }
+
+            p {
+                "Puppet: Lead Dev and Website Creator"
+            }
+
+            p {
+                "Lncvrt: Secondary Dev"
+            }
+        }
+    }
+}
+
+fn main() {
+    sycamore::render(App);
+}
